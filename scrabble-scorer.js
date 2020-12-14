fc2b52eefc9105b31b47aa00c0fdbe4ca4a7d2cb
Carly Langlois <cj.langlois.cl@gmail.com>
2020-12-14 10:27:13 -0600

    lcStudentCarly partial solution

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -13,73 +13,73 @@ const oldPointStructure = {
 };
 
 function initialPrompt() {
-//   console.log(`
-//    Welcome to the Scrabble score calculator.
-//    Which scoring algorithm would you like to use?
+  console.log(`
+   Welcome to the Scrabble score calculator.
+   Which scoring algorithm would you like to use?
 
-//    0 - Scrabble: The traditional scoring algorithm.
-//    1 - Simple Score: Each letter is worth 1 point.
-//    2 - Bonus Vowels: Vowels are worth 3 pts, and consonants are 1 pt.
+   0 - Scrabble: The traditional scoring algorithm.
+   1 - Simple Score: Each letter is worth 1 point.
+   2 - Bonus Vowels: Vowels are worth 3 pts, and consonants are 1 pt.
 
-//    Enter 0, 1, or 2:`);
+   Enter 0, 1, or 2:`);
 }
 
 function transform(lettersByScore) {
-//    const scoresByLetter = {};
-//    for (let score in lettersByScore) {
-//       let letters = lettersByScore[score];
-//       for (let i=0; i<letters.length; i++){
-//          scoresByLetter[letters[i].toLowerCase()] = Number(score);
-//       }         
-//    }
-//   return scoresByLetter;
+   const scoresByLetter = {};
+   for (let score in lettersByScore) {
+      let letters = lettersByScore[score];
+      for (let i=0; i<letters.length; i++){
+         scoresByLetter[letters[i].toLowerCase()] = Number(score);
+      }         
+   }
+  return scoresByLetter;
 }
 
-let newPointStructure;
-// let newPointStructure = transform(oldPointStructure);
+//let newPointStructure;
+let newPointStructure = transform(oldPointStructure);
 
 let simpleScore = function(word){
-   // return word.length;
+   return word.length;
 };
 
 let vowelBonusScore = function(word){
-   // let score = 0;
-   // let vowels = 'aeiou';
-   // for (let i = 0; i < word.length; i++){
-   //    if (vowels.includes(word[i].toLowerCase())){
-   //       score += 3;
-   //    } 
-   //    else {
-   //       score++;
-   //    }
-   // }
-   // return score;
+   let score = 0;
+   let vowels = 'aeiou';
+   for (let i = 0; i < word.length; i++){
+      if (vowels.includes(word[i].toLowerCase())){
+         score += 3;
+      } 
+      else {
+         score++;
+      }
+   }
+   return score;
 };
 
 let scrabbleScore = function(word, letterPoints) {
-   // let score = 0;
-   // for (let i = 0; i < word.length; i++){
-   //    score += letterPoints[word[i].toLowerCase()];
-   // }
-   // return score;
+   let score = 0;
+   for (let i = 0; i < word.length; i++){
+      score += letterPoints[word[i].toLowerCase()];
+   }
+   return score;
 };
 
 const scoringAlgorithms = [
-   // {
-   //    name: "",
-   //    description: "",
-   //    scoringFunction: simpleScore
-   // },
-   // {
-   //    name: "",
-   //    description: "",
-   //    scoringFunction: vowelBonusScore
-   // },
-   // {
-   //    name: "",
-   //    description: "",
-   //    scoringFunction: scrabbleScore
-   // }
+   {
+      name: "",
+      description: "",
+      scoringFunction: simpleScore
+   },
+   {
+      name: "",
+      description: "",
+      scoringFunction: vowelBonusScore
+   },
+   {
+      name: "",
+      description: "",
+      scoringFunction: scrabbleScore
+   }
 ];
 
 
