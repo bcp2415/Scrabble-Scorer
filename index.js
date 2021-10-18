5f667d055074308c5f109b42ff7980ce27728a0f
mprinceps <64213121+bcp2415@users.noreply.github.com>
2021-10-18 23:06:30 +0000

    This version passes all 17 Jasmine tests, but at cost of commenting out one line that solved (I think) the bonus problem (allowing blank tiles as well as letter tiles in words

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// This version passes all 17 Jasmine tests, but at cost of commenting out one line that solved (I think) the bonus problem (allowing blank tiles as well as letter tiles in words).
 
 var Jasmine = require('jasmine');
 var jasmine = new Jasmine();
